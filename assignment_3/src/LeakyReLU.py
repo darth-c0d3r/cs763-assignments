a638f6e48cf7e59ec1495629d64419f824b261d4
tnikhil <nikhiltummidi2002@gmail.com>
2019-02-14 23:14:37 +0530

    Leaky ReLU

---
+++ assignment_3/src/LeakyReLU.py
@@ -0,0 +1,21 @@
+import torch
+
+class LeakyReLU:
+
+	def __init__():
+		self.alpha = 0.01
+		self.output = None
+		self.gradInput = None
+	
+	def forward(self, inp):
+		# if input = n x d
+		alph_inp = inp * self.alpha
+		self.output = torch.max(inp, alph_inp)
+		return self.output
+
+	def backward(self, inp, gradOutput):
+		DomByDon = self.output
+		DomByDon[> 0] = 1
+		DomByDon[< 0] = self.alpha
+		self.gradInput = gradOutput * DomByDon
+		return self.gradInput
