d5dea2a3be5223331038685dd0056ee638a1b01a
Gurparkash <gurparkashsohi@gmail.com>
2019-02-16 01:49:31 +0530

    assignment_3: momentum added

--- assignment_3/src/Optimizer.py
+++ assignment_3/src/Optimizer.py
@@ -1,12 +1,13 @@
 import torch
 
 def GradientDescent(layer, lr):
+	# len(lr) = 1
 	layer.W -= lr[0] * layer.gradW
 	layer.B -= lr[0] * layer.gradB
 
 def GradientDescentWithMomentum(layer, lr):
+	# len(lr) = 2
 	layer.momW = lr[0]*layer.momW + lr[1]*layer.gradW
 	layer.momB = lr[0]*layer.momB + lr[1]*layer.gradB
 	layer.W -= layer.momW
 	layer.B -= layer.momB
-
