32f182358c3e211207b70c1f0b6e6f702b3840b9
tnikhil <nikhiltummidi2002@gmail.com>
2019-02-16 23:51:23 +0530

    dropout

---
+++ assignment_3/src/Dropout.py
@@ -0,0 +1,29 @@
+import torch
+
+class Dropout:
+	def __init__(self, dropout_rate, isTrain=True):
+		self.dropout_rate = dropout_rate
+		self.not_dropped = None
+		self.output = None
+		self.gradInput = None
+		self.isTrain = isTrain
+
+	def forward(self, inp):
+		self.output = inp
+		if (self.isTrain):
+			rand_vals = torch.rand((1, inp.shape[1])).double() # values in [0,1]
+			self.not_dropped = (rand_vals > self.dropout_rate).double()
+			self.output *= self.not_dropped/self.dropout_rate
+		return self.output
+
+	def backward(self, inp, gradOutput, lr=None):
+		self.gradInput = gradOutput
+		if (self.isTrain):
+			self.gradInput *= self.not_dropped/self.dropout_rate
+		return self.gradInput
+
+	def set_device(self, device):
+		pass
+
+	def set_optim(self, optim):
+		pass
