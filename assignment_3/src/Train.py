98fad0e5d467e1d95bb0347d17e4b11be9c0d5df
Gurparkash <gurparkashsohi@gmail.com>
2019-02-19 22:45:16 +0530

    printing routine improved

--- assignment_3/src/Train.py
+++ assignment_3/src/Train.py
@@ -17,15 +17,15 @@ input_data = normalize_data(input_data)
 train_data, train_labels, val_data, val_labels = split_data(input_data, input_labels)
 
 batch_size = 500
-epochs = 200
+epochs = 50
 
 # lr = [0.9,0.05] # lr, friction
 lr = [0.9, 0.01]
 model = Model(lr, "GradientDescentWithMomentum")
-model.addLayer(Linear(train_data.shape[1], 10))
+model.addLayer(Linear(train_data.shape[1], 1024))
 # model.addLayer(BatchNorm1D(1024))
-# model.addLayer(ReLU())
-# model.addLayer(Linear(1024, 512))
+model.addLayer(ReLU())
+model.addLayer(Linear(1024, 6))
 # model.addLayer(BatchNorm1D(512))
 # model.addLayer(ReLU())
 # model.addLayer(Linear(512, 256))
@@ -35,6 +35,10 @@ model.addLayer(Linear(train_data.shape[1], 10))
 model.set_device(device)
 
 for epoch in range(epochs):
+
+	loss_total = float(0)
+	num_samples = 0
+	num_correct = 0
 
 	for i in range(int(math.ceil(train_data.shape[0]/batch_size))):
 
@@ -48,4 +52,20 @@ for epoch in range(epochs):
 		loss = CrossEntropy.forward(out, target)
 		model.backward(CrossEntropy.backward(out, target))
 		model.setLearningRate(lr)
-		print("Epoch = %d : Loss = %f : Accuracy = %d/%d" % (epoch, loss, accuracy, data.shape[0]))
+
+		loss_total += loss
+		num_samples += data.shape[0]
+		num_correct += accuracy
+
+	print("[T] Epoch = %d : Loss = %f : Accuracy = %d/%d (%f)" % (epoch, loss_total/num_samples, num_correct, 
+				num_samples, num_correct * 100.0 / num_samples ))
+
+	out = model.forward(val_data)
+	pred = torch.max(out, 1)[1]
+	accuracy = torch.sum(pred.reshape(-1) == val_labels.reshape(-1))
+	loss = CrossEntropy.forward(out, val_labels)
+
+	print("[V] Epoch = %d : Loss = %f : Accuracy = %d/%d (%f)" % (epoch, loss, accuracy, 
+				val_data.shape[0], accuracy * 100.0 / val_data.shape[0] ))
+
+	print("")
