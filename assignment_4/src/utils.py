891f7b37d9170baf279f9dc4a556e68628dced8b
tnikhil <nikhiltummidi2002@gmail.com>
2019-03-17 00:15:08 +0530

    one hot

--- assignment_4/src/utils.py
+++ assignment_4/src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def get_data(filename):
 	with open(filename) as file:
@@ -7,3 +8,14 @@ def get_data(filename):
 	data = np.array([np.array([0]*(max_len - len(row)) + row) for row in data])
 	return data
 
+def one_hot_encode(X, k):
+	N, D = X.shape
+	X_new =  np.zeros((N, D, k))
+	for i in range(N):
+		for d in range(D):
+			X_new[i, d, X[i, d]] = 1
+	return X_new
+
+def hist(filename):
+	fl = pd.read_csv(filename)
+	his = pd.DataFrame(fl).hist(bins=10)
